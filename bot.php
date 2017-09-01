01c71729ada78d5fd11d8373a1d97d532192b339
thongtank <thongtank@hotmail.com>
2017-09-01 12:01:43 +0700

    bot v8

--- bot.php
+++ bot.php
@@ -5,17 +5,16 @@ require __DIR__ . '/vendor/autoload.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('El29kUlVlDnk43OQWxmUu3pLbqEB6vx3PGS5qwloangM7/dW/Z5A0qZ9Eua++8ExUlQrz9iHWzFBIXmgqrRS16wlwDcOPWwx+IzfWasHux2sNlrBc048VTNTZAa6eLFKIg2dD3uSPKu9j6+C/XBbqwdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'eead3f759f457b2e8d3e711a074ef8d0']);
 
-// $content = file_get_contents('php://input');
+$content = file_get_contents('php://input');
 
 // // parse to JSON data
-// $data = json_decode($content, true);
+$data = json_decode($content, true);
 
-// $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-// $response = $bot->replyMessage($data['events']['replyToken'], $textMessageBuilder);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage($data['events']['replyToken'], $textMessageBuilder);
 
-// echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-
-// exit();
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+exit();
 
 // // // รับค่าจาก request body content
 // // $content = file_get_contents('php://input');
