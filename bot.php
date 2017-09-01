2d1e2bddfb86a19181043e573629b1aad260753c
thongtank <thongtank@hotmail.com>
2017-09-01 11:40:07 +0700

    create bot reply

--- bot.php
+++ bot.php
@@ -2,14 +2,18 @@
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('El29kUlVlDnk43OQWxmUu3pLbqEB6vx3PGS5qwloangM7/dW/Z5A0qZ9Eua++8ExUlQrz9iHWzFBIXmgqrRS16wlwDcOPWwx+IzfWasHux2sNlrBc048VTNTZAa6eLFKIg2dD3uSPKu9j6+C/XBbqwdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'eead3f759f457b2e8d3e711a074ef8d0']);
 
-// รับค่าจาก request body content
-$content = file_get_contents('php://input');
-
-// parse to JSON data
-$data = json_decode($content, true);
-
-$file = fopen("event.txt","w");
-fwrite($file, $data);
-fclose($file);
+$response = $bot->replyText('<reply token>', 'hello!');
 
 exit();
+
+// // รับค่าจาก request body content
+// $content = file_get_contents('php://input');
+
+// // parse to JSON data
+// $data = json_decode($content, true);
+
+// $file = fopen("event.txt","w");
+// fwrite($file, $data);
+// fclose($file);
+
+// exit();
