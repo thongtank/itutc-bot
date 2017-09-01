e54b8c1e23989b127c11cef7f59e0cc5a389490e
thongtank <thongtank@hotmail.com>
2017-09-01 11:45:13 +0700

    create bot reply v'2

--- bot.php
+++ bot.php
@@ -2,7 +2,15 @@
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('El29kUlVlDnk43OQWxmUu3pLbqEB6vx3PGS5qwloangM7/dW/Z5A0qZ9Eua++8ExUlQrz9iHWzFBIXmgqrRS16wlwDcOPWwx+IzfWasHux2sNlrBc048VTNTZAa6eLFKIg2dD3uSPKu9j6+C/XBbqwdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'eead3f759f457b2e8d3e711a074ef8d0']);
 
-$response = $bot->replyText('<reply token>', 'hello!');
+$content = file_get_contents('php://input');
+
+// parse to JSON data
+$data = json_decode($content, true);
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->replyMessage($data['events']['replyToken'], $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 
 exit();
 
