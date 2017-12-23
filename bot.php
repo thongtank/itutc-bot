a09ee09394ccf0a2e65f0be4ce136ff7b01ffd04
thongtank <thongtank@hotmail.com>
2017-12-24 00:50:22 +0700

    CALL API 9

--- bot.php
+++ bot.php
@@ -48,10 +48,7 @@ $replyToken = $data['replyToken'];
 $topic = trim($data['message']['text']);
 $query_string = 'tp=' . urlencode($topic);
 // $replyMessage = $query_string;
-$json = file_get_contents('http://139.99.5.183/~tonglineat/get_reply.php?' . $query_string);
-// $replyMessage = $json;
-$replyMessage = json_decode($json)
-// $replyMessage = $replyMessage['message'];
+$replyMessage = file_get_contents('http://139.99.5.183/~tonglineat/get_reply.php?' . $query_string);
 
 $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($replyMessage);
 $response = $bot->replyMessage($replyToken, $textMessageBuilder);
