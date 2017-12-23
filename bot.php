6a40a6078128766b3c5649465018a524d460f120
thongtank <thongtank@hotmail.com>
2017-12-24 00:31:08 +0700

    CALL API 6

--- bot.php
+++ bot.php
@@ -47,8 +47,9 @@ $replyToken = $data['replyToken'];
 
 $topic = trim($data['message']['text']);
 $query_string = 'tp=' . urlencode($topic);
-$replyMessage = $query_string;
-// $json = file_get_contents('http://139.99.5.183/~tonglineat/get_reply.php?' . $query_string);
+// $replyMessage = $query_string;
+$json = file_get_contents('http://139.99.5.183/~tonglineat/get_reply.php?' . $query_string);
+$replyMessage = $json;
 // $replyMessage = json_decode($json, true)
 // $replyMessage = $replyMessage['reply_msg'];
 
